eb4b527b35e1e7386289e8cacc3c5e43258172b7
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-09-24 19:20:27 +0200

    made fs and path

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 import { reverseString, countCharacters, charString } from "./stringUtils";
+import fs from "fs";
+import path from 'path';
+
+const reversed = reverseString("You can gather and modify date in the database");
+fs.writeFileSync('reversed.txt', reversed);
+
+const filePath:string = path.join( __dirname, 'reversed.txt');
+console.log(`Sentence saved at: ${filePath}`);
+
 
 console.log( reverseString("You can gather and modify date in the database"));
 console.log( countCharacters("dynamic content"));
