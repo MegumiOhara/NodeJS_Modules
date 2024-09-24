e4122b9a633e0f1bb185bf07fb18b50e6bedb3e6
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-09-24 17:21:43 +0200

    added npx ts init

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { reverseString, countCharacters, sliceString } from "./stringUtils";
+
+console.log( reverseString("You can gather and modify date in the database"));
+console.log( countCharacters("dynamic content"));
+console.log( sliceString("NodeJS"));
---
+++ stringUtils.ts
@@ -0,0 +1,11 @@
+export const reverseString = (str:string):string => {
+    return str.split('').reverse().join('');
+};
+
+export const countCharacters = (str:string):number => {
+    return str.length;
+};
+
+export const sliceString = (str:string):string => {
+    return str.slice();
+};
